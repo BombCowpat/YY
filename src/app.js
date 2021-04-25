00981c708261199cdbd9b0b4cfd9b0d9d0240d01
chenyanfei <15873624283@163.com>
2021-04-25 23:31:51 +0800

    feat:router

--- src/app.js
+++ src/app.js
@@ -1,80 +1,18 @@
 const http = require('http')
-const ejs = require('ejs')
-const path = require('path')
 const { static } = require('./middleware/staticMid')
 const { getReqUrlObj } = require('./utils/common')
+
+const { router } = require('./router')
 
 const app = http.createServer(async(req,res) => {
   let next = await static(req, res, '../../../static')
   if(next) {
     let urlObj = getReqUrlObj(req)
     let pathname = urlObj.pathname
-    if(pathname === '/news') { 
-      let msg = 'name:yy age:18'
-      let list = [
-        {
-          a:1,
-          b:2
-        },
-        {
-          a:11,
-          b:22
-        },
-        {
-          a:123,
-          b:232
-        },
-        {
-          a:124,
-          b:255
-        }
-      ]
-      ejs.renderFile(path.resolve(__dirname,'./views/news.ejs'), {msg,list}, (err, data) => {
-        if(err) {
-          console.log(err)
-          res.writeHead(500, {
-            'Content-Type': 'text/plain'
-          })
-          res.end(JSON.stringify(err))
-        }else {
-          res.writeHead(200, {
-            'Content-Type': 'text/html'
-          })
-          res.end(data) 
-        }
-
-      })
-    } else if (pathname === '/login'){
-      ejs.renderFile(path.resolve(__dirname, './views/login.ejs'), {}, (err, data)=> {
-        if(err) {
-          res.writeHead(500, {
-            'Content-Type': 'text/plain'
-          })
-          res.end(JSON.stringify(err))
-        }else {
-          res.writeHead(200, {
-            'Content-Type': 'text/html'
-          })
-          res.end(data) 
-        }
-      })
-    } else if(pathname === '/dologin') {
-      let data = ''
-      req.on('data', (chunk) => {
-        data += chunk
-      })
-      req.on('end', () => {
-        res.writeHead(200, {
-          'Content-Type': 'text/html'
-        })
-        res.end(data) 
-      })
-    }
-    else {
-      res.writeHead(404, {
-        'Content-Type': 'text/plain;charset=utf-8'
-      })
-      res.end('404 Not Found!')
+    if(typeof router[pathname] === 'function') {
+      router[pathname](req, res)
+    }else {
+      router.notFound(req, res)
     }
   }
 })
---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+const ejs = require('ejs')
+const path = require('path')
+
+exports.router = {
+  '/news': function (req, res) {
+    let msg = 'name:yy age:18'
+    let list = [
+      {
+        a: 1,
+        b: 2,
+      },
+      {
+        a: 11,
+        b: 22,
+      },
+      {
+        a: 123,
+        b: 232,
+      },
+      {
+        a: 124,
+        b: 255,
+      },
+    ]
+    ejs.renderFile(path.resolve(__dirname, '../views/news.ejs'), { msg, list }, (err, data) => {
+      if (err) {
+        console.log(err)
+        res.writeHead(500, {
+          'Content-Type': 'text/plain',
+        })
+        res.end(JSON.stringify(err))
+      } else {
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+        })
+        res.end(data)
+      }
+    })
+  },
+  '/login': function (req, res) {
+    ejs.renderFile(path.resolve(__dirname, '../views/login.ejs'), {}, (err, data) => {
+      if (err) {
+        res.writeHead(500, {
+          'Content-Type': 'text/plain',
+        })
+        res.end(JSON.stringify(err))
+      } else {
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+        })
+        res.end(data)
+      }
+    })
+  },
+  '/dologin': function (req, res) {
+    let data = ''
+    req.on('data', chunk => {
+      data += chunk
+    })
+    req.on('end', () => {
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+      })
+      res.end(data)
+    })
+  },
+  notFound: function (req, res) {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain;charset=utf-8',
+    })
+    res.end('404 Not Found!')
+  },
+}
